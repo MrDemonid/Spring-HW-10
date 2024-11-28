4ab16d977a77d8ae4bf0d457ca56029905399cb2
MrDemonid <MrDemonid@yandex.ru>
2024-11-29 00:43:16 +0300

    add tests for Catalog & Order services

--- Service-Catalog/src/test/java/mr/demonid/service/catalog/ServiceCatalogApplicationTests.java
+++ Service-Catalog/src/test/java/mr/demonid/service/catalog/ServiceCatalogApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+//@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class ServiceCatalogApplicationTests {
 
     @Test
